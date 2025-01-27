df2399a9c05c03d2fdcf6e29a50dc24ca57d8eb0
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-01-27 20:20:47 +0530

    Do some changes

--- AssignmentApplication/AssignmentApplication/src/main/java/com/_6/AssignmentApplication/service/AssignmentService.java
+++ AssignmentApplication/AssignmentApplication/src/main/java/com/_6/AssignmentApplication/service/AssignmentService.java
@@ -18,9 +18,28 @@ public class AssignmentService {
     public Assignment save(User user) {
         Assignment assignment = new Assignment();
         assignment.setStatus(AssignmentStatusEnum.PENDING_SUBMISSION.getStatus());
+        assignment.setNumber(findNextAssignmentToSubmit(user));
         assignment.setUser(user);
 
         return assignmentRepo.save(assignment);
+    }
+
+    private Integer findNextAssignmentToSubmit(User user) {
+        Set<Assignment> assignmentByUser = assignmentRepo.findByUser(user);
+        if(assignmentByUser == null){
+            return 1;
+        }
+        Optional<Integer> nextAssignmentOpt = assignmentByUser.stream().sorted((a1,a2) ->{
+            if(a1.getNumber() == null) return 1;
+            if(a2.getNumber() == null) return 1;
+            return a2.getNumber().compareTo(a1.getNumber());
+        })
+                .map(assignment -> {
+                    if( assignment.getNumber() == null )
+                        return 1;
+                    return assignment.getNumber() + 1;
+                }).findFirst();
+        return nextAssignmentOpt.orElse(1);
     }
 
     public Set<Assignment> findByUser(User user) {
